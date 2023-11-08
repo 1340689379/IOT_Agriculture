57ce3a469b75ad0b0b63fe33bb493cd4323f8036
zy-mine <1457678447@qq.com>
2023-11-08 13:31:18 +0800

    测试1.0上传

--- iot.cpp
+++ iot.cpp
@@ -22,7 +22,7 @@ Iot::~Iot()
 void Iot::onTimeOut(){
     QSqlQuery query;
     query.exec("select * from HUMIandTEMP1");
-    qDebug()<< query.value(0);
+    //qDebug()<< query.value(0);
     while(query.next()){
         QString time=query.value(0).toString();
         int temp=query.value(1).toInt();
@@ -30,6 +30,38 @@ void Iot::onTimeOut(){
         ui->time1->setText(time);
         ui->temp1->display(temp);
         ui->humi1->display(humi);
+    }
+    query.exec("select * from HUMIandTEMP2");
+    while(query.next()){
+        QString time=query.value(0).toString();
+        int temp=query.value(1).toInt();
+        int humi=query.value(2).toInt();
+        ui->time2->setText(time);
+        ui->temp2->display(temp);
+        ui->humi2->display(humi);
+    }
+    query.exec("select * from HUMIandTEMP3");
+    while(query.next()){
+        QString time=query.value(0).toString();
+        int temp=query.value(1).toInt();
+        int humi=query.value(2).toInt();
+        ui->time3->setText(time);
+        ui->temp3->display(temp);
+        ui->humi3->display(humi);
+    }
+    query.exec("select * from CO2");
+    while(query.next()){
+        QString time=query.value(0).toString();
+        int CO2=query.value(1).toInt();
+        ui->time4->setText(time);
+        ui->co2->display(CO2);
+    }
+    query.exec("select * from Light");
+    while(query.next()){
+        QString time=query.value(0).toString();
+        int light=query.value(1).toInt();
+        ui->time4->setText(time);
+        ui->light->display(light);
     }
 }
 
